58bfa1d7f224e76aee291d502919c58b7e7dd386
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2026-03-24 10:51:27 +0000

    Replace yaml cfg with dict in logging setup

--- src/paragraph_tts/utils/logging_utils.py
+++ src/paragraph_tts/utils/logging_utils.py
@@ -6,10 +6,7 @@ import pathlib
 from typing import Any
 from typing import Dict
 
-import yaml
-
 UTILITIES_HOME = pathlib.Path(__file__).absolute().parent.as_posix()
-LOGGING_CONFIG_PATH = os.path.join(UTILITIES_HOME, 'res', 'logging_cfg.yaml')
 
 
 def setup_logging(script_signature: str,
@@ -22,6 +19,7 @@ def setup_logging(script_signature: str,
     Args:
         script_signature: Name of the script from which the function is called. Will be used to
             determine the log file name.
+        output_dir: Directory where the log files will be stored.
     """
 
     logging_config = _get_logging_config(script_signature, output_dir)
@@ -31,28 +29,67 @@ def setup_logging(script_signature: str,
     logging.config.dictConfig(logging_config)
 
 
-def _get_logging_config(script_signature: str, output_dir: str) -> Dict[str, Any]:
+def _get_logging_config(script_signature: str,
+                        output_dir: str) -> Dict[str, Any]:
     """Creates a global logging configuration.
+
+    Two handlers are created:
+        - a console handler that outputs primary info logs to the console;
+        - a file handler that outputs all logs to a file.
+
+    The loggers are designed to disable all logs from 3rd party libraries, whose level is below
+        WARNING.
 
     Returns:
         Compiled configuration ready to be loaded as a configuration
         dictionary to the logging module.
     """
 
-    custom_formatters = {'_ColorFormatter': _ColorFormatter}
+    log_file_path = os.path.join(output_dir,
+                                 script_signature,
+                                 f'{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}.log')
 
-    with open(LOGGING_CONFIG_PATH, encoding='utf-8') as config_file:
-        config_dict: Dict[str, Any] = yaml.safe_load(config_file.read())
+    return {
+        'version': 1,
+        'loggers': {
+            'root': {
+                'level': 'WARNING',
+                'handlers': ['console_hand', 'file_hand'],
+            },
+            'paragraph_tts': {
+                'level': 'DEBUG',
+                'handlers': ['console_hand', 'file_hand'],
+                'propagate': False,
+            }
+        },
+        'handlers': {
+            'console_hand': {
+                'class': 'logging.StreamHandler',
+                'level': 'INFO',
+                'formatter': 'color_formatter',
+                'stream': 'ext://sys.stdout',
+            },
+            'file_hand': {
+                'class': 'logging.FileHandler',
+                'level': 'NOTSET',
+                'formatter': 'default_formatter',
+                'filename': log_file_path,
+                'encoding': 'utf-8',
+            },
+        },
+        'formatters': {
+            'color_formatter': {
+                '()': _ColorFormatter,
+                'format': '[%(asctime)s][%(levelname)s][%(name)s] %(message)s',
+                'datefmt': '%Y-%m-%d %H:%M:%S',
+            },
+            'default_formatter': {
+                'format': '[%(asctime)s][%(levelname)s][%(name)s] %(message)s',
+                'datefmt': '%Y-%m-%d %H:%M:%S',
+            },
+        },
 
-    for _, formatter in config_dict['formatters'].items():
-        if '()' in formatter and formatter['()'] in custom_formatters:
-            formatter['()'] = custom_formatters[formatter['()']]
-
-    timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-    out_file_path = f'{output_dir}/{script_signature}/{timestamp}.log'
-    config_dict['handlers']['file_hand']['filename'] = out_file_path
-
-    return config_dict
+    }
 
 
 class _ColorFormatter(logging.Formatter):
@@ -83,8 +120,10 @@ if __name__ == '__main__':
 
     setup_logging('logging_utils')
 
-    logging.debug('This is a debug message.')
-    logging.info('This is an info message.')
-    logging.warning('This is a warning message.')
-    logging.error('This is an error message.')
-    logging.critical('This is a critical message.')
+    logger = logging.getLogger('paragraph_tts.utils.logging_utils')
+
+    logger.debug('This is a debug message.')
+    logger.info('This is an info message.')
+    logger.warning('This is a warning message.')
+    logger.error('This is an error message.')
+    logger.critical('This is a critical message.')
